60718e18d2ae1dbc39f4420a0cfda58948a44982
Marcin Fundalewicz <marcin.fundalewicz@gmail.com>
2024-01-09 15:02:31 +0100

    commit 3

--- src/main/java/pl/javastart/task/HeadPhones.java
+++ src/main/java/pl/javastart/task/HeadPhones.java
@@ -4,11 +4,13 @@ public class HeadPhones {
     String brandName;
     String colour;
     String size;
+
     HeadPhones(String brandName, String colour, String size) {
         this.brandName = brandName;
         this.colour = colour;
         this.size = size;
     }
+
     void showInfo() {
         System.out.println("Brand: " + brandName + " Colour: " + colour + " Size: " + size);
     }
